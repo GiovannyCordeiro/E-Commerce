3184e5637725e0f1329e80e2be24c0217ae94e71
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2022-11-16 14:04:19 -0300

    :construction: small header features

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,27 @@
 import "../public/styles/style.scss";
-console.log("salve")
+
+class Header{
+  private openMenu = document.getElementById("menu-mobile")?.addEventListener("click", this.openMenuMobile.bind(this));
+  private closeMenu = document.getElementById("close-menu")?.addEventListener("click", this.closeMenuMobile.bind(this));
+  private linksMobile = document.getElementById("wrapper-links");
+
+  openMenuMobile(){
+    this.linksMobile!.style.display = "block"
+  };
+
+  closeMenuMobile(){
+    this.linksMobile!.style.display = "none"
+  };
+};
+
+class App{
+  constructor(){
+
+  };
+
+  static init(){
+    const header = new Header();
+  };
+}
+
+App.init();
