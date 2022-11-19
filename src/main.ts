ef8f382477fd17b894da2de2ae1533efd3f7d3cd
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2022-11-19 14:36:01 -0300

    :sparkles: produc image

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,49 @@ class Header{
   openShoppingCard(){
     const stateShoppingCard = this.shoppingCard?.classList;
     stateShoppingCard?.toggle("active")
-  }
+  };
 };
+
+class Main{
+  images = [
+    "../assets/imagens/image-product-1.jpg", 
+    "../assets/imagens/image-product-2.jpg", 
+    "../assets/imagens/image-product-3.jpg", 
+    "../assets/imagens/image-product-4.jpg"
+  ];
+
+  imgProd = document.getElementById("img-prod");
+  arrowRight = document.getElementById("arrow-right")?.addEventListener("click", this.nextImage.bind(this));
+  arrowLeft = document.getElementById("arrow-left")?.addEventListener("click", this.prevImage.bind(this));
+  num:number = 0;
+
+  handlerImage(numberImage:number){
+    this.imgProd!.style.backgroundImage = `url(${this.images[numberImage]})`;
+    this.imgProd!.style.backgroundPosition = "center 0";
+  }
+
+  nextImage(){
+    if(this.num < 3){
+      this.num++;
+      this.handlerImage(this.num);
+    }
+    else{
+      this.num = 0;
+      this.handlerImage(this.num);
+    }
+  }
+  
+  prevImage(){
+    if(this.num === 0){
+      this.num = 3;
+      this.handlerImage(this.num);
+    }
+    else{
+      this.num--;
+      this.handlerImage(this.num);
+    }
+  }
+}
 
 class App{
   constructor(){
@@ -28,6 +69,7 @@ class App{
 
   static init(){
     const header = new Header();
+    const main = new Main();
   };
 }
 
