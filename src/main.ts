8cfdbf5e4a047fbd1e40a3825ea43ebb06813cfe
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2022-11-25 20:59:41 -0300

    :ok_hand: item shop card

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import "../public/styles/style.scss";
 class Header{
   openMenu = document.getElementById("menu-mobile")?.addEventListener("click", this.openMenuMobile.bind(this));
   closeMenu = document.getElementById("close-menu")?.addEventListener("click", this.closeMenuMobile.bind(this));
-  linksMobile = document.getElementById("wrapper-links");
   iconCarShop = document.getElementById("icon-car-shop")?.addEventListener("click", this.openShoppingCard.bind(this));
+  linksMobile = document.getElementById("wrapper-links");
   shoppingCard = document.getElementById("shopping-card");
 
   openMenuMobile(){
