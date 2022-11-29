bb7311f5e0032dc0e4f088ff571d161d7d9d9a0e
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2022-11-29 17:38:44 -0300

    :sparkles: Maximized Product Desktop

--- src/main.ts
+++ src/main.ts
@@ -29,19 +29,38 @@ class Main{
     "../assets/imagens/image-product-4.jpg"
   ];
 
-  imgProd = document.getElementById("img-prod");
+  imgProdImg = document.getElementById("img-prod");
+  imgProd = document.getElementById("img-prod")?.addEventListener("click", this.openMaxProd.bind(this));
   arrowRight = document.getElementById("arrow-right")?.addEventListener("click", this.nextImage.bind(this));
   arrowLeft = document.getElementById("arrow-left")?.addEventListener("click", this.prevImage.bind(this));
   num:number = 0;
+  
+  maximizedProduct = document.getElementById("maximized-product");
+  
+  //Maximized Desktop Images
+  mainImgMax = document.getElementById("main-img-max");
+  
+  thumbOneMax = document.getElementById("thumb-max-1")?.addEventListener("click", () => {this.modifImgMainMax(0)});
+  thumbTwoMax = document.getElementById("thumb-max-2")?.addEventListener("click", () => {this.modifImgMainMax(1)});
+  thumbThreeMax = document.getElementById("thumb-max-3")?.addEventListener("click", () => {this.modifImgMainMax(2)});
+  thumbFourMax = document.getElementById("thumb-max-4")?.addEventListener("click", () => {this.modifImgMainMax(3)});
 
+  arrowLeftMax = document.getElementById("arrow-left-max")?.addEventListener("click", () => {this.imgMainBaseArrow("Left")});
+  arrowRightMax = document.getElementById("arrow-right-max")?.addEventListener("click", () => {this.imgMainBaseArrow("Right")});
+
+  closeMaximizedProduct = document.getElementById("close-window-maxprod")?.addEventListener("click", this.closeMaxProd.bind(this));
+
+  numImgMainMax = 0;
+  
+  //Counter Items
+  counterDOM = document.getElementById("counter");
   iconPlus = document.getElementById("icon-plus")?.addEventListener("click", this.incrementQuantItem.bind(this));
   iconMinus = document.getElementById("icon-minus")?.addEventListener("click", this.decrementQuantItem.bind(this));
-  counterDOM = document.getElementById("counter");
   counter:number = 0;
 
   handlerImage(numberImage:number){
-    this.imgProd!.style.backgroundImage = `url(${this.images[numberImage]})`;
-    this.imgProd!.style.backgroundPosition = "center 0";
+    this.imgProdImg!.style.backgroundImage = `url(${this.images[numberImage]})`;
+    this.imgProdImg!.style.backgroundPosition = "center 0";
   }
 
   nextImage(){
@@ -77,6 +96,46 @@ class Main{
       this.counter = 0
     }
     this.counterDOM!.innerText = `${this.counter}`;
+  }
+
+  openMaxProd(){
+    this.maximizedProduct?.classList.add("active");
+  }
+
+  closeMaxProd(){
+    this.maximizedProduct?.classList.remove("active");
+  }
+
+  modifImgMainMax(number: number){
+    this.numImgMainMax = number;
+    this.mainImgMax!.style.backgroundImage = `url(${this.images[number]})`;
+    this.mainImgMax!.style.backgroundSize = "cover";
+  }
+
+  imgMainBaseArrow(position:String){
+    if(position === "Left"){
+      if(this.numImgMainMax === 0){
+        this.numImgMainMax = 3;
+        this.handlerImageBaseArrow(this.numImgMainMax)
+      }
+      else{
+        this.numImgMainMax--;
+        this.handlerImageBaseArrow(this.numImgMainMax);
+      }
+    }
+    else{
+      if(this.numImgMainMax >= 3){
+        this.handlerImageBaseArrow(this.numImgMainMax = 0)
+      }
+      else{
+        this.numImgMainMax++
+        this.handlerImageBaseArrow(this.numImgMainMax);
+      }
+    }
+  }
+
+  handlerImageBaseArrow(num:number){
+    this.mainImgMax!.style.backgroundImage = `url(${this.images[num]})`
   }
 }
 class App{
