9b3c1180c088e7a03e90e5d68fe50b5d0c3f80df
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2022-12-01 16:48:01 -0300

    :recycle: Rename variables

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "../public/styles/style.scss";
 class Header{
   openMenu = document.getElementById("menu-mobile")?.addEventListener("click", this.openMenuMobile.bind(this));
   closeMenu = document.getElementById("close-menu")?.addEventListener("click", this.closeMenuMobile.bind(this));
-  iconCarShop = document.getElementById("icon-car-shop")?.addEventListener("click", this.openShoppingCard.bind(this));
+  iconCarShop = document.getElementById("icon-car-shop")?.addEventListener("click", this.stateShoppingCard.bind(this));
   linksMobile = document.getElementById("wrapper-links");
   shoppingCard = document.getElementById("shopping-card");
 
@@ -15,7 +15,7 @@ class Header{
     this.linksMobile!.classList.remove("active");
   };
 
-  openShoppingCard(){
+  stateShoppingCard(){
     const stateShoppingCard = this.shoppingCard?.classList;
     stateShoppingCard?.toggle("active")
   };
@@ -31,14 +31,12 @@ class Main{
 
   //mobile handler
   imgProdListener = document.getElementById("img-prod")?.addEventListener("click", this.openMaxProd.bind(this));
-  arrowRight = document.getElementById("arrow-right")?.addEventListener("click", this.nextImage.bind(this));
-  arrowLeft = document.getElementById("arrow-left")?.addEventListener("click", this.prevImage.bind(this));
-  numMobileHandler:number = 0;
+  arrowRight = document.getElementById("arrow-right")?.addEventListener("click", () => {this.toggleImageMob("next")});
+  arrowLeft = document.getElementById("arrow-left")?.addEventListener("click", () => {this.toggleImageMob("prev")});
+  numHandlerImgMob:number = 0;
   
   //handler product main 
   imgProduct = document.getElementById("img-prod");
-  
-  numberHandlerMain = 0;
 
   thumbsMain = [
     document.getElementById("thumb-main-1"),
@@ -68,41 +66,41 @@ class Main{
 
   arrowLeftMax = document.getElementById("arrow-left-max")?.addEventListener("click", () => {this.imgMainBaseArrow("Left")});
   arrowRightMax = document.getElementById("arrow-right-max")?.addEventListener("click", () => {this.imgMainBaseArrow("Right")});
+  closeMaxProduct = document.getElementById("close-window-maxprod")?.addEventListener("click", this.closeMaxProd.bind(this));
 
-  closeMaximizedProduct = document.getElementById("close-window-maxprod")?.addEventListener("click", this.closeMaxProd.bind(this));
-
-  numImgMainMax = 0;
+  imgChangeNumberMax = 0;
   
   //Counter Items
+  counter:number = 0;
   counterDOM = document.getElementById("counter");
   iconPlus = document.getElementById("icon-plus")?.addEventListener("click", this.incrementQuantItem.bind(this));
   iconMinus = document.getElementById("icon-minus")?.addEventListener("click", this.decrementQuantItem.bind(this));
-  counter:number = 0;
 
   handlerImage(numberImage:number){
     this.imgProduct!.style.backgroundImage = `url(${this.images[numberImage]})`;
     this.imgProduct!.style.backgroundPosition = "center 0";
   }
 
-  nextImage(){
-    if(this.numMobileHandler < 3){
-      this.numMobileHandler++;
-      this.handlerImage(this.numMobileHandler);
+  toggleImageMob(mode:string){
+    if(mode === "next"){
+      if(this.numHandlerImgMob < 3){
+        this.numHandlerImgMob++;
+        this.handlerImage(this.numHandlerImgMob);
+      }
+      else{
+        this.numHandlerImgMob = 0;
+        this.handlerImage(this.numHandlerImgMob);
+      }
     }
     else{
-      this.numMobileHandler = 0;
-      this.handlerImage(this.numMobileHandler);
-    }
-  }
-  
-  prevImage(){
-    if(this.numMobileHandler === 0){
-      this.numMobileHandler = 3;
-      this.handlerImage(this.numMobileHandler);
-    }
-    else{
-      this.numMobileHandler--;
-      this.handlerImage(this.numMobileHandler);
+      if(this.numHandlerImgMob === 0){
+        this.numHandlerImgMob = 3;
+        this.handlerImage(this.numHandlerImgMob);
+      }
+      else{
+        this.numHandlerImgMob--;
+        this.handlerImage(this.numHandlerImgMob);
+      }
     }
   }
 
@@ -128,29 +126,29 @@ class Main{
   }
 
   modifImgMainMax(number: number){
-    this.numImgMainMax = number;
+    this.imgChangeNumberMax = number;
     this.mainImgMax!.style.backgroundImage = `url(${this.images[number]})`;
     this.mainImgMax!.style.backgroundSize = "cover";
   }
 
   imgMainBaseArrow(position:String){
     if(position === "Left"){
-      if(this.numImgMainMax === 0){
-        this.numImgMainMax = 3;
-        this.handlerImageBaseArrow(this.numImgMainMax)
+      if(this.imgChangeNumberMax === 0){
+        this.imgChangeNumberMax = 3;
+        this.handlerImageBaseArrow(this.imgChangeNumberMax)
       }
       else{
-        this.numImgMainMax--;
-        this.handlerImageBaseArrow(this.numImgMainMax);
+        this.imgChangeNumberMax--;
+        this.handlerImageBaseArrow(this.imgChangeNumberMax);
       }
     }
     else{
-      if(this.numImgMainMax >= 3){
-        this.handlerImageBaseArrow(this.numImgMainMax = 0)
+      if(this.imgChangeNumberMax >= 3){
+        this.handlerImageBaseArrow(this.imgChangeNumberMax = 0)
       }
       else{
-        this.numImgMainMax++
-        this.handlerImageBaseArrow(this.numImgMainMax);
+        this.imgChangeNumberMax++
+        this.handlerImageBaseArrow(this.imgChangeNumberMax);
       }
     }
   }
