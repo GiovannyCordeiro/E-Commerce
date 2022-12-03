a02931c34cdfea1e5451672d27ae04044ce29c9d
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2022-12-03 13:08:29 -0300

    :sparkles: send itens to shopping card

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import "../public/styles/style.scss";
 class Header{
   openMenu = document.getElementById("menu-mobile")?.addEventListener("click", this.openMenuMobile.bind(this));
   closeMenu = document.getElementById("close-menu")?.addEventListener("click", this.closeMenuMobile.bind(this));
-  iconCarShop = document.getElementById("icon-car-shop")?.addEventListener("click", this.stateShoppingCard.bind(this));
   linksMobile = document.getElementById("wrapper-links");
-  shoppingCard = document.getElementById("shopping-card");
 
   openMenuMobile(){
     this.linksMobile!.classList.add("active");
@@ -14,12 +12,39 @@ class Header{
   closeMenuMobile(){
     this.linksMobile!.classList.remove("active");
   };
+};
 
+class ShoppingCard{
+  shoppingCard = document.getElementById("shopping-card");
+  iconCarShop = document.getElementById("icon-car-shop")?.addEventListener("click", this.stateShoppingCard.bind(this));
+
+  static listProducts:{id:number,count:number}[] = [];
+  
   stateShoppingCard(){
     const stateShoppingCard = this.shoppingCard?.classList;
     stateShoppingCard?.toggle("active")
   };
-};
+
+  injectItemShopCard(item:{}, number:number){
+    console
+  }
+
+  static generateItemShop(){
+    const newItem  = 
+      `
+      <div class="product-card">
+        <div class="thumb-card"></div>
+        <div class="description-card">
+          <p>Autumn Limited Edition...</p>
+          <div class="price">
+            <p>$125.00 x 3</p><b>$375,00</b>
+          </div>
+        </div>
+        <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
+      </div>
+      `;
+  }
+}
 
 class Main{
   images = [
@@ -108,7 +133,7 @@ class Main{
     this.counter++;
     this.counterDOM!.innerText = `${this.counter}`;
   }
-
+  
   decrementQuantItem(){
     this.counter--;
     if(this.counter < 0){
@@ -164,10 +189,34 @@ class Main{
     this.handlerImage(num);
     this.thumbsMain[num]?.classList.add("active");
   }
+
+  //shopping
+
+  btnAddShoppinngCard = document.getElementById("btn-add-shoppinng-card")?.addEventListener("click", this.addListProducts.bind(this));
+  addListProducts(){
+    const productItem = {
+      id: 1,
+      description: "salve",
+      price: 302,
+      discont: 10,
+      count: this.counter,
+    }
+    const filter = ShoppingCard.listProducts.filter((element) => {return element.id === productItem.id});
+    const index = ShoppingCard.listProducts.findIndex((element) => {return element.id === productItem.id});
+    if(index !== -1){
+      filter[0].count = this.counter
+      ShoppingCard.listProducts.splice(index,1,filter[0]);
+      // console.log(ShoppingCard.listProducts)
+      return;
+    }
+    ShoppingCard.listProducts.push(productItem);
+    // console.log(ShoppingCard.listProducts)
+  }
 }
 class App{
   static init(){
     const header = new Header();
+    const shoppingCard = new ShoppingCard();
     const main = new Main();
   };
 }
